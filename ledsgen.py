a6afc61f3f3c081f1d28a497b9f6996672a321e4
luvwahraan <luvwahraan+git@pikaed.org>
2024-10-31 15:40:31 +0100

    formatting output

--- ledsgen.py
+++ ledsgen.py
@@ -3,4 +3,5 @@
 for col in range(1, 10):
   for row in range(0, 2):
     print( f"CC {col}{row} 0 | 0x90 0x19 0x00")
-    print( f"CC {col}{row} 127 | 0x90 0x19 0x7F\n") 
+    print( f"CC {col}{row} 127 | 0x90 0x19 0x7F")
+  print('')
